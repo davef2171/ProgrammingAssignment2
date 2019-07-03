7deabbe9df56ccd1058aba96db9dfe8b097a9e87
Davef2171 <fosterdw@shaw.ca>
2019-07-02 18:30:10 -0700

    Initial code update

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Twin functions to a) create a complex object which contains a variable
+## structure of a matrix, its inverse and two operators set, get for the value and its inverse value; and
+## b) to return either a valid cached value or in the case on no valid cache set it.
+## Limitations - Matrix must be invertible
 
-## Write a short comment describing this function
+## makeCacheMatrix create a complex variable and the operators which work on it.
+## values - a matrix and its inverse
+## operators - set, get matrix;  setinverse, getinverse on the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inv <<- solve
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## Presumes makecacheMatrix has been called on x 
+## Then verifies the cached inverse value returning it if valid and, if not sets it
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...) {
+  ## x is a Cached Matrix 
+  ## Return a matrix that is the inverse of matrix in 'x'
+  ## Check if there is an inverse
+  data <- x$get()
+  inv <- x$getinverse()
+  
+  if (!is.null(inv) ){
+    
+    ## Still same values and inverse is cached
+    return(inv)
+  }
+  
+  ## cache is invalid or not set
+  inv <- solve(data,...)
+  x$setinverse(inv)
+  inv
+}
+
+## Presumes x has been created with makecacheMatrix
+## Then verifies the cached inverse value returning it if valid and, if not sets it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		## Check matrix is still consistent as they have equal dimensions
+		## and if there is an inverse
+		inv <- x$getinverse()
+		data <- x$get()
+		
+		if ( all(x == data) $$ !is.null(inv) ){
+		
+			## Still same values and inverse is cached
+			return(inv)
+			}
+		
+		## cache is invalid or not set
+		inv <- solve(data,...)
+		x$setinverse(data)
+		data
+		}
 }
